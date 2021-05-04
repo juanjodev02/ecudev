7ab73fd341b88e499f34abdd4f80876c43bb081a
Juanjo Jaramillo <juan.jaramillo02@epn.edu.ec>
2021-05-03 22:40:06 -0500

    apollo provider added 🚀

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { ApolloProvider } from '@apollo/client'
+
+import theme from '../utils/theme'
+import { AppProps } from 'next/app'
+import { useApollo } from '../apollo/client'
+import { Layout } from '../components/Layout'
+
+function MyApp ({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps.initialApolloState)
+  return (
+    <ApolloProvider client={apolloClient}>
+      <ChakraProvider resetCSS theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </ApolloProvider>
+  )
+}
+
+export default MyApp
